99fb30d1eceef154c76ce4c72a6295eb3bf3f035
Jackson <jackson_rhoden@outlook.com>
2024-10-28 16:32:45 +0000

    feat: added FindLongestWords and CalculateLetterFrequency method functionality and testing

--- java-junit/src/main/java/org/example/WordAnalyser.java
+++ java-junit/src/main/java/org/example/WordAnalyser.java
@@ -1,15 +1,47 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordAnalyser {
-    public String[] findLongestWords(String text) {
+    public static String[] findLongestWords(String text) {
         // TODO: Implement the logic to find the longest word(s) in the given text
-        return new String[]{""};
+        String[] output = text.split(" ");
+        String[] currentLongestWordsArray = {output[0]}; ;
+        int currentLongestWordLength = currentLongestWordsArray[0].length();
+        for (int i = 0  ;  i < output.length ; i++){
+            if ( output[i].length() == currentLongestWordLength ){
+                currentLongestWordsArray[currentLongestWordsArray.length - 1] = output[i];
+            }
+            if (output[i].length() > currentLongestWordLength){
+                currentLongestWordsArray = new String[]{""};
+                currentLongestWordsArray[0] = output[i];
+
+            }
+            currentLongestWordLength = currentLongestWordsArray[0].length();
+        }
+        return currentLongestWordsArray;
     }
 
-    public Map<Character, Integer> calculateLetterFrequency(String text) {
+    public static Map<Character, Integer> calculateLetterFrequency(String text) {
         // TODO: Implement the logic to calculate the frequency of each letter in the given text
-        return null;
+        String lowerCaseText = text.toLowerCase();
+        String noSpacesText = lowerCaseText.replaceAll("[-+.?@$!%^:,\\s]","");
+        char[] lettersArray = noSpacesText.toCharArray();
+        HashMap<Character, Integer> letterFrequency = new HashMap<>();
+
+        letterFrequency.put(lettersArray[0], 1);
+
+        for (int i = 1 ; i < noSpacesText.length() ; i++){
+            if (letterFrequency.containsKey(lettersArray[i])){
+                letterFrequency.put(lettersArray[i], letterFrequency.get(lettersArray[i]) + 1) ;
+
+            }else {
+                letterFrequency.put(lettersArray[i], 1) ;
+            }
+        }
+
+        return letterFrequency;
     }
 }
--- java-junit/src/test/java/org/example/WordAnalyserTest.java
+++ java-junit/src/test/java/org/example/WordAnalyserTest.java
@@ -2,15 +2,41 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordAnalyserTest {
 
     @Test
-    void findLongestWords() {
+    void testFindLongestWords() {
+        String testSentence1 = "This is a cool test sentence with a really long word.";
+
+        String[] result1 = WordAnalyser.findLongestWords(testSentence1);
+
+        String[] expectedResult1 = {"sentence"};
+
+        assertTrue(Arrays.equals(expectedResult1,result1));
     }
 
     @Test
-    void calculateLetterFrequency() {
+    void testCalculateLetterFrequency() {
+
+        Map<Character, Integer> expectedResult1 = Map.of('a',3, 'b', 2);
+        Map<Character, Integer> expectedResult2 = Map.of('a',3, 'b', 2, 'c', 1, 'd', 1);
+        Map<Character, Integer> expectedResult3 = Map.of('a',3, 'b', 1, 'c', 5);
+
+
+        Map<Character, Integer> result1 = WordAnalyser.calculateLetterFrequency("aaa bb");
+        Map<Character, Integer> result2 = WordAnalyser.calculateLetterFrequency("A bad cab!");
+        Map<Character, Integer> result3 = WordAnalyser.calculateLetterFrequency("AAA@$B  CCCCC");
+
+
+        assertEquals(expectedResult1, result1);
+        assertEquals(expectedResult2, result2);
+        assertEquals(expectedResult3, result3);
+
+
     }
 }
