5611ab58337337ec5513b42cf960ec08710204a6
Joel Cohen <jcohenf50@gmail.com>
2024-10-28 12:18:12 +0000

    feat: rotate method accounts for left turns. Tests passing

--- java-junit/src/main/java/org/example/Compass.java
+++ java-junit/src/main/java/org/example/Compass.java
@@ -5,20 +5,22 @@ public class Compass {
     public static Point rotate(Point startPoint, Direction newDirection){
         Point resultOutput = null;
 
-        if (startPoint.equals(Compass.Point.NORTH) && newDirection.equals(Compass.Direction.RIGHT)){
-            resultOutput = Compass.Point.EAST;
-        }
-        if (startPoint.equals(Point.SOUTH) && newDirection.equals(Compass.Direction.RIGHT)){
-            resultOutput = Point.WEST;
-        }
-        if (startPoint.equals(Point.EAST) && newDirection.equals(Compass.Direction.RIGHT)){
-            resultOutput = Point.SOUTH;
-        }
-        if (startPoint.equals(Point.WEST) && newDirection.equals(Compass.Direction.RIGHT)){
-            resultOutput = Point.NORTH;
+        if(newDirection == Direction.RIGHT) {
+            switch(startPoint) {
+                case NORTH -> resultOutput = Point.EAST;
+                case SOUTH -> resultOutput = Point.WEST;
+                case EAST -> resultOutput = Point.SOUTH;
+                case WEST -> resultOutput = Point.NORTH;
+            }
+        } else {
+            switch (startPoint) {
+                case NORTH -> resultOutput = Point.WEST;
+                case SOUTH -> resultOutput = Point.EAST;
+                case EAST -> resultOutput = Point.NORTH;
+                case WEST -> resultOutput = Point.SOUTH;
+            }
         }
         return resultOutput;
-
     }
 
     public enum Point {
--- java-junit/src/test/java/org/example/CompassTest.java
+++ java-junit/src/test/java/org/example/CompassTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CompassTest {
 
 
- /*   @Test
+  @Test
     @DisplayName("returns East when passed North Point and Right Direction ")
     // returns East when passed North Point and Right Direction
     public void compassFacingNorthCanRotateRight() {
@@ -19,12 +19,13 @@ class CompassTest {
 
         resultOutput = Compass.rotate(Compass.Point.NORTH, Compass.Direction.RIGHT);
         assertEquals(Compass.Point.EAST, resultOutput );
-    }*/
+    }
+
 
     @Test
     @DisplayName("Returns correct point when rotating right ")
     // returns East when passed North Point and Right Direction
-    public void compassFacingNorthCanRotateRight() {
+    public void compassCanRotateRight() {
         Compass.Point resultOutput;
 
         Compass.Point northResultOutput = Compass.rotate(Compass.Point.NORTH, Compass.Direction.RIGHT);
@@ -40,6 +41,29 @@ class CompassTest {
 
 
                 );
+
+
+    }
+
+    @Test
+    @DisplayName("Returns correct point when rotating right ")
+    // returns East when passed North Point and Right Direction
+    public void compassCanRotateLeft() {
+        Compass.Point resultOutput;
+
+        Compass.Point northResultOutput = Compass.rotate(Compass.Point.NORTH, Compass.Direction.LEFT);
+        Compass.Point southResultOutput = Compass.rotate(Compass.Point.SOUTH, Compass.Direction.LEFT);
+        Compass.Point eastResultOutput = Compass.rotate(Compass.Point.EAST, Compass.Direction.LEFT);
+        Compass.Point westResultOutput = Compass.rotate(Compass.Point.WEST, Compass.Direction.LEFT);
+
+        assertAll(
+                () -> assertEquals(Compass.Point.WEST, northResultOutput),
+                () -> assertEquals(Compass.Point.EAST, southResultOutput),
+                () -> assertEquals(Compass.Point.NORTH, eastResultOutput),
+                () -> assertEquals(Compass.Point.SOUTH, westResultOutput)
+
+
+        );
 
 
     }
